3089e721c8f08692014f4ca71d99c9f29c8d5794
jakepanlilio2000 <jakepanlilio2000@gmail.com>
2026-07-19 16:26:58 +0800

    WIP, corrected from vanilla

--- LocatorAutoPrint/Services/EditCountSheetService.cs
+++ LocatorAutoPrint/Services/EditCountSheetService.cs
@@ -23,9 +23,22 @@ namespace LocatorAutoPrint.Services
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                        SELECT SlotNo, RecNo, UPC, SKU, Descr, Qty, EditedQty 
-                        FROM PUREGOLD.dbo.COUNTSHEET 
-                        WHERE SlotNo = @slotNo AND RecNo = @recNo";
+                        SELECT
+                            SlotNo,
+                            RecNo,
+                            UPC,
+                            SKU,
+                            Descr,
+                            Qty,
+                            EditedQty,
+                            CountDate,
+                            Posted,
+                            Added,
+                            Edited,
+                            LastEditedQty
+                        FROM PUREGOLD.dbo.COUNTSHEET
+                        WHERE SlotNo = @slotNo
+                        AND RecNo = @recNo";
 
                     cmd.Parameters.AddWithValue("@slotNo", slotNo);
                     cmd.Parameters.AddWithValue("@recNo", recNo);
@@ -140,23 +153,43 @@ namespace LocatorAutoPrint.Services
                 using (var cmd = conn.CreateCommand())
                 {
                     cmd.CommandText = @"
-                        UPDATE PUREGOLD.dbo.COUNTSHEET 
-                        SET UPC = @upc, 
-                            SKU = @sku, 
-                            Descr = @descr, 
-                            EditedQty = @editedQty, 
-                            Edited = 1 
-                        WHERE SlotNo = @slotNo AND RecNo = @recNo";
+                        UPDATE PUREGOLD.dbo.COUNTSHEET
+                            SET
+                                RecNo = @newRecNo,
+                                UPC = @newUPC,
+                                SKU = @newSKU,
+                                Descr = @newDescr,
+                                EditedQty = @newEditedQty,
+                                Edited = 1
+                            WHERE
+                                SlotNo = @oldSlotNo
+                            AND RecNo = @oldRecNo
+                            AND CountDate = @oldCountDate
+                            AND UPC = @oldUPC
+                            AND SKU = @oldSKU
+                            AND Descr = @oldDescr
+                            AND Qty = @oldQty
+                            AND EditedQty = @oldEditedQty
+                            AND Posted = @oldPosted
+                            AND Added = @oldAdded
+                            AND Edited = @oldEdited
+                            AND LastEditedQty IS NULL;";
 
                     cmd.Parameters.AddWithValue("@upc", record.UPC ?? "");
-                    cmd.Parameters.AddWithValue("@sku", skuValue); // Using parsed numeric value
+                    cmd.Parameters.AddWithValue("@sku", skuValue); 
                     cmd.Parameters.AddWithValue("@descr", record.Descr ?? "");
                     cmd.Parameters.AddWithValue("@editedQty", record.EditedQty);
                     cmd.Parameters.AddWithValue("@slotNo", record.SlotNo);
                     cmd.Parameters.AddWithValue("@recNo", record.RecNo);
 
                     int rowsAffected = await cmd.ExecuteNonQueryAsync();
-                    return rowsAffected > 0;
+                    if (rowsAffected == 0)
+                    {
+                        throw new Exception(
+                            "The record was modified by another user. Please reload it before saving.");
+                    }
+
+                    return true;
                 }
             }
         }
